0d6d8cdba52518fe53572b9571157bd40f8ea705
Océane Lahoual <138216585+Midunighto@users.noreply.github.com>
2024-04-29 13:59:29 +0200

    fix: bug on pixel deletion, wrong argument was passed + svg style conflict with responsive

--- frontend/src/pages/Grid.jsx
+++ frontend/src/pages/Grid.jsx
@@ -11,6 +11,8 @@ import "../styles/grids.scss";
 
 import penImg from "../assets/paint.webp";
 import eraserImg from "../assets/eraser.webp";
+import bombBonusImg from "../assets/bomb.webp";
+import penBonusImg from "../assets/big-pen.webp";
 
 export default function Grid() {
   const { storedUser } = useStoredUser();
@@ -19,12 +21,21 @@ export default function Grid() {
   const [grid, setGrid] = useState([]);
   const { id } = useParams();
   const canvasRef = useRef(null);
-
+  /* CHRONO STATE */
+  const [startTime, setStartTime] = useState(null);
+  const [elapsedTime, setElapsedTime] = useState(0);
   /* DESSIN STATE */
   const [pen, setPen] = useState(false);
   const [eraser, setEraser] = useState(false);
   const [color, setColor] = useColor("#ffffff");
   const [lastPixelTime, setLastPixelTime] = useState(Date.now());
+  /* BONUS STATE */
+  const [userPixelCount, setUserPixelCount] = useState(0);
+  const [bombBonus, setBombBonus] = useState([{}, {}, {}]);
+  const [useBombBonus, setUseBombBonus] = useState(false);
+
+  const [penBonus, setPenBonus] = useState([{}, {}, {}, {}, {}]);
+  const [usePenBonus, setUsePenBonus] = useState(false);
 
   useEffect(() => {
     const loadData = async () => {
@@ -34,6 +45,7 @@ export default function Grid() {
         );
         setGridData(response.data);
         setGridName(response.data.name);
+        setStartTime(new Date(response.data.created_at));
 
         const pixelResponse = await axios.get(
           `${import.meta.env.VITE_BACKEND_URL}/api/grids/${id}/pixels`
@@ -45,16 +57,40 @@ export default function Grid() {
     };
     loadData();
   }, [id]);
+  /* CHRONO */
+  useEffect(() => {
+    if (startTime) {
+      const interval = setInterval(() => {
+        setElapsedTime(new Date() - startTime);
+      }, 1000);
 
+      return () => clearInterval(interval);
+    }
+  }, [startTime]);
+
+  function formatTime(timeInMilliseconds) {
+    const seconds = Math.floor(timeInMilliseconds / 1000);
+    const minutes = Math.floor(seconds / 60);
+    const hours = Math.floor(minutes / 60);
+
+    const formattedSeconds = `0${seconds % 60}`.slice(-2);
+    const formattedMinutes = `0${minutes % 60}`.slice(-2);
+    const formattedHours = `0${hours}`.slice(-2);
+
+    return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+  }
+
+  /* CANVA */
   const pixelSize = 11; // Taille de chaque "pixel" du damier
+  const nbPixels = 40;
   useEffect(() => {
     const canvas = canvasRef.current;
     if (canvas) {
       const ctx = canvas.getContext("2d");
 
       // Dessiner un damier
-      const numX = 40; // Nombre de "pixels" en largeur
-      const numY = 40; // Nombre de "pixels" en hauteur
+      const numX = nbPixels; // Nombre de "pixels" en largeur
+      const numY = nbPixels; // Nombre de "pixels" en hauteur
 
       for (let i = 0; i < numX; i++) {
         for (let j = 0; j < numY; j++) {
@@ -97,75 +133,99 @@ export default function Grid() {
   };
 
   /* INTÉRACTIVITÉ JEU */
-  const handleCanvasClick = async (event) => {
+  const applyBomb = () => {
+    setUseBombBonus(true);
+  };
+  const handleAddPixel = async (x, y) => {
+    const now = Date.now();
+    if (now - lastPixelTime < 5000) {
+      // Moins de 5 secondes se sont écoulées depuis le dernier pixel placé
+      return;
+    }
+
+    try {
+      const pixelData = {
+        user_id: storedUser.id,
+        grid_id: id,
+        color: color.hex,
+        x_coordinate: Math.floor(x / pixelSize),
+        y_coordinate: Math.floor(y / pixelSize),
+      };
+
+      const response = await axios.post(
+        `${import.meta.env.VITE_BACKEND_URL}/api/grids/${id}/pixels`,
+        pixelData,
+        { withCredentials: true }
+      );
+      setLastPixelTime(now);
+      setUserPixelCount((prevCount) => {
+        const newCount = prevCount + 1;
+        if (newCount === 70) {
+          setBombBonus((prevBonuses) => [...prevBonuses, {}]);
+          return 0;
+        } else {
+          return newCount;
+        }
+      });
+
+      setGrid((prevGrid) => [
+        ...prevGrid,
+        {
+          id: response.data.insertId,
+          color: color.hex,
+          x_coordinate: Math.floor(x / pixelSize),
+          y_coordinate: Math.floor(y / pixelSize),
+        },
+      ]);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleErasePixel = async (x, y) => {
+    try {
+      const pixelX = Math.floor(x / pixelSize);
+      const pixelY = Math.floor(y / pixelSize);
+
+      /* Vérifier si un pixel existe à ces coordonnées dans la grille */
+      const selectedPixel = grid.find(
+        (pixel) =>
+          pixel.x_coordinate === pixelX && pixel.y_coordinate === pixelY
+      );
+      console.log(selectedPixel);
+      console.log(selectedPixel.id);
+      if (selectedPixel && selectedPixel.id !== undefined) {
+        /* on supprime le pixel de la grid front  */
+        setGrid((prevGrid) =>
+          prevGrid.filter((pixel) => pixel.id !== selectedPixel.id)
+        );
+
+        await axios.delete(
+          `${import.meta.env.VITE_BACKEND_URL}/api/grids/${id}/pixels/${
+            selectedPixel.id
+          }`,
+          { withCredentials: true }
+        );
+        const pixelResponse = await axios.get(
+          `${import.meta.env.VITE_BACKEND_URL}/api/grids/${id}/pixels`
+        );
+        setGrid(pixelResponse.data);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleCanvasClick = (event) => {
     const canvas = canvasRef.current;
-    const ctx = canvas.getContext("2d");
     const rect = canvas.getBoundingClientRect();
-    const scale = 0.8; // 0.8 car transform: scale(8) en css
     const x = event.clientX - rect.left;
     const y = event.clientY - rect.top;
 
     if (pen) {
-      const now = Date.now();
-      if (now - lastPixelTime < 5000) {
-        // Moins de 5 secondes se sont écoulées depuis le dernier pixel placé
-        return;
-      }
-      ctx.fillStyle = color.hex;
-      ctx.fillRect(
-        Math.floor(x / pixelSize) * pixelSize,
-        Math.floor(y / pixelSize) * pixelSize,
-        pixelSize,
-        pixelSize
-      );
-
-      try {
-        const pixelData = {
-          user_id: storedUser.id,
-          grid_id: id,
-          color: color.hex,
-          x_coordinate: Math.floor(x / pixelSize),
-          y_coordinate: Math.floor(y / pixelSize),
-        };
-
-        const response = await axios.post(
-          `${import.meta.env.VITE_BACKEND_URL}/api/grids/${id}/pixels`,
-          pixelData,
-          { withCredentials: true }
-        );
-        setLastPixelTime(now);
-      } catch (err) {
-        console.error(err);
-      }
+      handleAddPixel(x, y);
     } else if (eraser) {
-      try {
-        const pixelX = Math.floor(x / pixelSize);
-        const pixelY = Math.floor(y / pixelSize);
-
-        /* Vérifier si un pixel existe à ces coordonnées dans la grille */
-        const selectedPixel = grid.find(
-          (pixel) =>
-            pixel.x_coordinate === pixelX && pixel.y_coordinate === pixelY
-        );
-
-        if (selectedPixel) {
-          await axios.delete(
-            `${import.meta.env.VITE_BACKEND_URL}/api/grids/${id}/pixels/${
-              selectedPixel.id
-            }`,
-            { withCredentials: true }
-          );
-          /* on supprime le pixel de la grid front aussi */
-          setGrid((prevGrid) =>
-            prevGrid.filter(
-              (pixel) =>
-                pixel.x_coordinate !== pixelX || pixel.y_coordinate !== pixelY
-            )
-          );
-        }
-      } catch (err) {
-        console.error(err);
-      }
+      handleErasePixel(x, y);
     }
   };
 
@@ -187,6 +247,9 @@ export default function Grid() {
     <div className="page area-page">
       <section className="grid-container">
         <div className="header">
+          <div className="chrono-container">
+            <p>{formatTime(elapsedTime)}</p>
+          </div>
           <form onSubmit={handleSubmit}>
             <h1>
               <input type="text" placeholder={gridName} onChange={handleName} />
@@ -197,13 +260,39 @@ export default function Grid() {
           </form>
         </div>
         <div className="grid-area-container">
-          <canvas
-            ref={canvasRef}
-            width={40 * pixelSize}
-            height={40 * pixelSize}
-            className="canva"
-            onClick={handleCanvasClick}
-          />
+          <div className="game-wrapper">
+            <div className="canva-container">
+              <canvas
+                ref={canvasRef}
+                width={nbPixels * pixelSize}
+                height={nbPixels * pixelSize}
+                className="canva"
+                onClick={handleCanvasClick}
+              />
+            </div>
+            <div className="bonus">
+              <div className="bombs-container">
+                {bombBonus.length > 0 &&
+                  bombBonus.map((bonus, index) => (
+                    <Button
+                      key={index}
+                      className="bomb-bonus"
+                      type="button"
+                      onClick={applyBomb}
+                    >
+                      <img src={bombBonusImg} alt="" width={30} height={30} />
+                      <p>1</p>
+                    </Button>
+                  ))}
+              </div>
+              <div className="pens-container">
+                <Button className="pen-bonus">
+                  <img src={penBonusImg} alt="" width={30} height={30} />
+                  <p>5</p>
+                </Button>
+              </div>
+            </div>
+          </div>
           <div className="paint">
             <div className="tools">
               <Button
