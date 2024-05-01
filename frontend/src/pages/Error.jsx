f71046cefbf663a64c20537c7a48324c86e26c80
Océane Lahoual <138216585+Midunighto@users.noreply.github.com>
2024-05-01 04:05:05 +0200

    last fixes + animations

--- frontend/src/pages/Error.jsx
+++ frontend/src/pages/Error.jsx
@@ -1,13 +1,16 @@
 import { Link } from "react-router-dom";
 
+import whitelogo from "../assets/logo-white.png";
+
 export default function Error() {
   return (
-    <div className="wrapper">
-      <div className="container">
+    <div className="page">
+      <section className="error">
+        <img src={whitelogo} alt="logo" width={276} height={201} />
         <h1>Erreur 404</h1>
         <p>La page que vous recherchez n'existe pas</p>
         <Link to="/">Retour à l'accueil</Link>
-      </div>
+      </section>
     </div>
   );
 }
