263f9a4ddf7bdf2cabbd5588830c3d32d5a885e2
Océane Lahoual <138216585+Midunighto@users.noreply.github.com>
2024-05-04 12:51:07 +0200

    fix pixel info datetime to match real hour

--- frontend/src/components/PixelInfo.jsx
+++ frontend/src/components/PixelInfo.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 const PixelInfo = ({ pixelInfos, clickX, clickY, pixelSize }) => {
   const date = new Date(pixelInfos.createdAt);
+  date.setHours(date.getHours() - 2);
   const dateString = date.toLocaleDateString("fr-FR", {
     year: "numeric",
     month: "numeric",
